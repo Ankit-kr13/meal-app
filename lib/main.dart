f888d1faac1e0256e91d2e4ff3d05552c800f0e5
ANKIT KUMAR <ankitwork139@gmail.com>
2024-05-07 16:18:16 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals/categories.dart';
+
+// final theme = ThemeData(
+//   useMaterial3: true,
+//   colorScheme: ColorScheme.fromSeed(
+//     brightness: Brightness.dark,
+//     seedColor: const Color.fromARGB(255, 131, 57, 0),
+//   ),
+//   textTheme: GoogleFonts.latoTextTheme(),
+// );
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(),
+      home: const categoriesScreen(),
+    );
+  }
+}
